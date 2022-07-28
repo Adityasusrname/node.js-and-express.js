94159a85a8d51ef37a9d37c10ffce8958b14ebdc
Aditya Srivastava <adityasmailbooxx@gmail.com>
2022-07-29 02:00:46 +0530

    currently on fileSync

---
+++ modules.js
@@ -0,0 +1,10 @@
+const names=require('./names')
+const sayHi=require('./utils')
+const data=require('./alternative-flavours')
+console.log(data)
+
+sayHi('Aditya')
+sayHi(names.john)
+sayHi(names.peter)
+
+
